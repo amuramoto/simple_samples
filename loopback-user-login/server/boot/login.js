e5353ab84c83d86d6924d513549c3a09c51e9cf7
Alex Muramoto <amuramoto@apache.org>
2016-04-27 11:17:08 -0700

    Adds example logging and res status codes

--- loopback-user-login/server/boot/login.js
+++ loopback-user-login/server/boot/login.js
@@ -8,27 +8,44 @@ module.exports = function (app) {
 			"username": req.body.username,
 			"password": req.body.password
 		}
-		UserModel.login(userCredentials, 'user', function (err, access_token) {
+		UserModel.login(userCredentials, 'user', function (err, result) {			
 			if (err) {
+				Log.error(err);
 				res.status(401).json({"error": "login failed"});
 				return;
 			}
-			res.json(access_token);
+
+			Log.info({
+				"username": userCredentials.username,
+				"timestamp": new Date.getTime(),
+				"action": "login"
+			});
+
+			const access_token = result.id;
+			const ttl = result.ttl;
+			res.json({
+				"token": access_token,
+				"ttl": ttl
+			});
 		});
 	});
 
 	app.get('/logout', function (req, res, next) {
 		const access_token = req.query.access_token;
 		if (!access_token) {
-			res.status(400).json({"error": "access token required"})
+			res.status(400).json({"error": "access token required"});
+			return;
 		}
 		UserModel.logout(access_token, function (err) {
 			if (err) {
+				Log.error({
+					"error": err,
+					"timestamp": new Date.getTime()
+				});
 				res.status(404).json({"error": "logout failed"});
 				return;
 			}
-			res.send("user has been logged out");
-			
+			res.status(204);
 		});		
 	});
 }
